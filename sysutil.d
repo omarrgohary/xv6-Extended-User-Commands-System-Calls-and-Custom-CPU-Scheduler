996324e69d5dc533a99d3c8c6d606884030050c4
Omar El Gohary <omarrmgohary@gmail.com>
2025-05-19 00:21:32 +0300

    Delete sysutil.d

--- sysutil.d
+++
@@ -1,4 +0,0 @@
-kernel/sysutil.o: kernel/sysutil.c \
- /usr/riscv64-linux-gnu/include/stdc-predef.h kernel/types.h \
- kernel/riscv.h kernel/datetime.h kernel/proc.h kernel/param.h \
- kernel/spinlock.h kernel/defs.h kernel/memlayout.h
