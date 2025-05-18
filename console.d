358dcd305a9956369bd78beb9e715158da9dbb9a
Omar El Gohary <omarrmgohary@gmail.com>
2025-05-19 00:08:25 +0300

    Delete console.d

--- console.d
+++
@@ -1,5 +0,0 @@
-kernel/console.o: kernel/console.c \
- /usr/riscv64-linux-gnu/include/stdc-predef.h kernel/types.h \
- kernel/riscv.h kernel/defs.h kernel/param.h kernel/memlayout.h \
- kernel/spinlock.h kernel/sleeplock.h kernel/fs.h kernel/file.h \
- kernel/proc.h
